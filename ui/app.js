abddeda4acf82c839a05321183f219fe3c8307cb
Harney <8xo@lap00091497.ornl.gov>
2013-11-12 14:44:44 -0500

    Cosmetic changes

--- ui/app.js
+++ ui/app.js
@@ -105,6 +105,62 @@ app.get('/users/:user_id',function(request,response) {
 });
 
 //jobs proxy service
+app.get('/apps',function(request,response) {
+  var options = {
+	host: 'localhost',
+	port: servicePort,
+	path: '/apps',
+	method: 'GET'
+  };
+	  
+  console.log('in service side /apps');
+  
+  var responseData = '';
+	  
+  var req = http.request(options, function(res) {
+    console.log('STATUS: ' + res.statusCode);
+	res.setEncoding('utf8');
+	res.on('data', function (chunk) {
+	  responseData += chunk;	
+    });
+	     
+    res.on("end", function () {
+    	
+    	
+    	console.log('apps responseData:\n ' + responseData);
+    	
+    	/*
+    	var data = {
+    	};
+    	
+    	console.log('apps: ' );//+ data['jobs'][0]['user_id']);
+    	
+    	
+    	
+    // you can use res.send instead of console.log to output via express
+    	var jsonObj = JSON.parse(responseData);
+        //response.send(jsonObj);
+        */
+    	var data = {'app':'aaa'};
+    	response.send(data);
+    }); 
+	     
+	      
+  });
+	  
+  req.on('error', function(e) {
+    console.log('problem with request: ' + e.message);
+  });
+   
+  // write data to request body
+  req.write('data\n');
+  req.write('data\n');
+  req.end();
+});
+
+
+
+//jobs proxy service
 app.get('/jobs',function(request,response) {
   var options = {
 	host: 'localhost',
@@ -134,7 +190,7 @@ app.get('/jobs',function(request,response) {
     	
     	console.log('jobs: ' + data['jobs'][0]['user_id']);
     	
-    	//console.log('responseData:\n ' + responseData);
+    	console.log('jobs responseData:\n ' + responseData);
     	
     // you can use res.send instead of console.log to output via express
     	var jsonObj = JSON.parse(responseData);
